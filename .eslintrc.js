842fd43cab63dc48d8a882b0151abbd6148c487e
johsam <j.samuelson64@gmail.com>
2017-10-02 08:39:20 +0200

    Initial release

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+    env: {
+        browser: true,
+        es6: true,
+        jquery: true,
+    },
+
+    globals: {
+        Ext: true,
+    },
+
+    extends: ['eslint:recommended', 'plugin:flowtype/recommended', 'airbnb'],
+    plugins: ['extjs', 'html', 'flowtype'],
+    parserOptions: {
+        sourceType: 'module',
+    },
+    rules: {
+        indent: ['error', 4],
+        'linebreak-style': ['error', 'unix'],
+        semi: ['error', 'always'],
+        'no-var': 0,
+        'vars-on-top': 0,
+        'space-before-function-paren': 0,
+        'object-shorthand': 0,
+        'prefer-template': 0,
+        'spaced-comment': 0,
+        'prefer-rest-params': 0,
+        'prefer-arrow-callback': 0,
+        'no-mixed-operators': 0,
+        'no-underscore-dangle': 0,
+        'no-shadow-restricted-names': 0,
+        'max-len': ['error', 170],
+        quotes: ['error', 'single', { avoidEscape: true }],
+        'func-names': ['error', 'never'],
+    },
+
+    settings: {
+        flowtype: {
+            onlyFilesWithFlowAnnotation: true,
+        },
+    },
+};
